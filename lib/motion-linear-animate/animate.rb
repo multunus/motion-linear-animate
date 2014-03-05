8cdff60b503791394d69825e5f1fc184d53f7699
Jerry <jerry.j@multunus.com>
2014-03-05 17:14:06 +0530

    initial commit

---
+++ lib/motion-linear-animate/animate.rb
@@ -0,0 +1,12 @@
+class LinearAnimator
+  def linear_animate(view, to: final_point, duration: time_period, completion: completion_block)
+    frame = view.frame
+    frame.origin.x = final_point.x
+    frame.origin.y = final_point.y
+    UIView.animateWithDuration(time_period,
+                               animations: lambda{
+                                 view.frame = frame
+                               },
+                               completion: completion_block)
+  end
+end
